fd44f60b8cc9019330ba199b8dabbef31ae9e2a9
re <re@solydxk>
2016-09-18 10:53:17 +0300

    Second commit

--- js/todos.js
+++ js/todos.js
@@ -1,154 +1,176 @@
 var todoContainer = document.getElementById('todo-container'),
-    inputForm = document.getElementById('input-form'),
     todoInput = document.getElementById('new-todo'),
     todoExample = document.getElementsByClassName('todo-example')[0],
     controlPanel = document.getElementsByClassName('control-panel')[0],
     checkAllBox = controlPanel.children[0],
-    itemsLeft = controlPanel.children[1],
-    showAllButton = controlPanel.children[2],
-    showUncompletedButton = controlPanel.children[3],
-    showCompletedButton = controlPanel.children[4],
-    removeCompletedButton = controlPanel.children[5],
-    todoNotes;
+    itemsLeftField = controlPanel.children[1],
+    showAllButton = document.querySelector('.control-panel #all'),
+    showUncompletedButton = document.querySelector('.control-panel #uncompleted'),
+    showCompletedButton = document.querySelector('.control-panel #completed'),
+    removeCompletedButton = document.querySelector('.control-panel #remove-completed');
 
-inputForm.addEventListener('submit', receiveNewTodo);
+var listState = (function() {
+  var show = 'all',
+      uncompleted = 0, completed = 0, total = 0; 
+
+  function filter(option) {
+    return option ? show = option : show;
+  }
+  function all(plusminus) {
+    return plusminus ? total += plusminus : total;
+  }
+  function left(plusminus) {
+    return plusminus ? uncompleted += plusminus : uncompleted;
+  }
+  return { filter: filter, left: left, all: all };
+})();
+
+todoInput.addEventListener('keypress', handleNewTodo);
 checkAllBox.addEventListener('change', checkAllToggle);
 showUncompletedButton.addEventListener('click', showUncompleted);
 showCompletedButton.addEventListener('click', showCompleted);
 showAllButton.addEventListener('click', showAll);
 removeCompletedButton.addEventListener('click', removeCompleted);
 
-var filter = (function() {
-  var state = 'all';
-  return { 'state': state };
-})();
+function updateItemsLeftField() {
+  itemsLeftField.innerHTML = 'Liko ' + listState.left();
+}
 
-var todosCounter = (function() {
-  var completed = 0,
-      all = 0;
-  return { completed: completed, all: all };
-})();
+function getTodos() {
+  return Array.from(document.querySelectorAll('#todo-container .todo-note'));
+}
+
+function setCompleted(todo) {
+  todo.children[0].checked = true;
+  todo.classList.add('completed');
+}
+
+function setUncompleted(todo) {
+  todo.children[0].checked = false;
+  todo.classList.remove('completed');
+}
+
+function isCompleted(todo) {
+  return todo.children[0].checked;
+}
+
+function handleNewTodo(keypress) {
+  if(keypress.key === 'Enter' && /\S/.test(todoInput.value)) {
+    var todo = receiveNewTodo();
+    todoInput.value = '';
+    setListeners(todo);
+    insertTodo(todo);
+  }
+}
 
 function receiveNewTodo() {
   var newTodo = todoExample.cloneNode(true),
-      checkbox = newTodo.children[0],
-      noteField = newTodo.children[1],
-      closeButton = newTodo.children[2];
-  newTodo.classList.add('todo-note');
+      noteField = newTodo.children[1];
   noteField.innerHTML = todoInput.value;
-  checkbox.addEventListener('change', completedToggle(checkbox));
-  closeButton.addEventListener('click', closeTodo(newTodo));
-  insertTodo(newTodo);
+  return newTodo;
+}
+
+function setListeners(todo) {
+  var checkbox = todo.children[0],
+      closeButton = todo.children[2];
+  checkbox.addEventListener('change', function() { completedToggle(todo); }); 
+  closeButton.addEventListener('click', function() { closeTodo(todo); });
 }
 
 function insertTodo(todo) {
-  if(/\S/.test(todoInput.value)) {
+  todo.classList.add('todo-note');
+  if(listState.filter() === 'completed')
+    todo.style.display = 'none';
   todoContainer.appendChild(todo);
   checkAllBox.checked = false;
-  todosCounter.all++;
-  itemsLeft.innerHTML = 'Liko ' + (todosCounter.all - todosCounter.completed);
-  console.log(todo);
-  todoInput.value = '';
-  }
+  listState.left(+1);
+  listState.all(+1);
+  updateItemsLeftField();
 }
 
-function showAll() {
-  todoNotes = Array.from(document.querySelectorAll('#todo-container .todo-note'));
-  filter.state = 'all';
-  todoNotes.forEach(function(note) {
-    note.style.display = 'block';
-  });
-}
-
-function showUncompleted() {
-  todoNotes = Array.from(document.querySelectorAll('#todo-container .todo-note'));
-  filter.state = 'uncompleted';
-  todoNotes.forEach(function(note) {
-    if(note.children[0].checked)
-      note.style.display = 'none'; 
-    else 
-      note.style.display = 'block';
-  });
-}
-
-function showCompleted() {
-  todoNotes = Array.from(document.querySelectorAll('#todo-container .todo-note'));
-  filter.state = 'completed';
-  todoNotes.forEach(function(note) {
-    if(note.children[0].checked)
-      note.style.display = 'block'; 
-    else
-      note.style.display = 'none'; 
-  });
-}
-
-function closeTodo(todo) {
-  return function() {
-    if(todo.children[0].checked === false) 
-      itemsLeft.innerHTML = 'Liko ' + (--todosCounter.all - todosCounter.completed);
-    else {
-      todosCounter.all--;
-      todosCounter.completed--;
+function completedToggle(todo) {
+  todo.classList.toggle('completed');
+  if(isCompleted(todo)) {
+    if(listState.left(-1) === 0) { 
+      checkAllBox.checked = true;
     }
-    todo.parentNode.removeChild(todo);
-  };
-}
-
-function completedToggle(checkbox) {
-  return function() {
-      checkbox.parentNode.classList.toggle('completed');
-      if(checkbox.checked) {
-        todosCounter.completed++;
-        if(todosCounter.all === todosCounter.completed) 
-          checkAllBox.checked = true;
-      }
-      else {
-        todosCounter.completed--;
-        checkAllBox.checked = false;
-      }
-      itemsLeft.innerHTML = 'Liko ' + (todosCounter.all - todosCounter.completed);
-  };
-}
-
-function checkAllToggle() {
-  todoNotes = Array.from(document.querySelectorAll('#todo-container .todo-note'));
-  if(checkAllBox.checked) {
-    todoNotes.forEach(function(note) {
-      note.children[0].checked = true;
-      note.classList.add('completed');
-    });
-    todosCounter.completed = todoNotes.length;
   }
   else {
-    todoNotes.forEach(function(note) {
-      note.children[0].checked = false;
-      note.classList.remove('completed');
-    });
-    todosCounter.completed = 0;
+    listState.left(+1);
+    checkAllBox.checked = false;
   }
   updateView();
 }
 
-function updateView() {
-    if (filter.state === 'all')
-      showAll();
-    else if (filter.state === 'completed')
-      showCompleted();
-    else
-      showUncompleted();
-    itemsLeft.innerHTML = 'Liko ' + (todosCounter.all - todosCounter.completed);
+function checkAllToggle() {
+  var todoNotes = getTodos();
+  if(checkAllBox.checked) {
+    todoNotes.forEach(setCompleted);
+    listState.left(-listState.left());
+  }
+  else {
+    todoNotes.forEach(setUncompleted);
+    listState.left(-listState.left() +listState.all());
+  }
+  updateView();
+}
+
+function closeTodo(todo) {
+  if(!isCompleted(todo)) { 
+    listState.left(-1);
+    updateItemsLeftField();
+  }
+  removeTodo(todo);
+}
+
+function removeTodo(todo) {
+  todo.parentNode.removeChild(todo);
+  listState.all(-1);
 }
 
 function removeCompleted() {
-  todoNotes = Array.from(document.querySelectorAll('#todo-container .todo-note'));
-  todoNotes.forEach(function(note) {
-    if(note.children[0].checked) {
-      todosCounter.completed--;
-      todosCounter.all--;
-      note.parentNode.removeChild(note); 
-    }
-  itemsLeft.innerHTML = 'Liko ' + (todosCounter.all - todosCounter.completed);
+  getTodos().forEach(function(todo) {
+    if(isCompleted(todo))
+      removeTodo(todo);
   });
   checkAllBox.checked = false;
 }
 
+function updateView() {
+  switch(listState.filter()) {
+    case 'all': showAll(); break;
+    case 'uncompleted': showUncompleted(); break;
+    case 'completed': showCompleted();
+  }
+  updateItemsLeftField();
+}
+
+function switchFilter(filter) {
+var previous = listState.filter(); 
+  if(previous !== filter) {
+    document.querySelector('.control-panel ' + '#' + previous).classList.remove('pressed');
+    document.querySelector('.control-panel ' + '#' + filter).classList.add('pressed');
+    listState.filter(filter);
+  }
+}
+
+function showAll() {
+  switchFilter('all');
+  getTodos().forEach(function(todo) {
+    todo.style.display = 'block';
+  });
+}
+
+function showUncompleted() {
+  switchFilter('uncompleted');
+  getTodos().forEach(function(todo) {
+    todo.style.display = isCompleted(todo) ? 'none' : 'block';
+  });
+}
+
+function showCompleted() {
+  switchFilter('completed');
+  getTodos().forEach(function(todo) {
+    todo.style.display = isCompleted(todo) ? 'block' : 'none';
+  });
+}
